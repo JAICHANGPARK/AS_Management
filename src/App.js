e2798032a908151ae2acc5e5c2345b8f89f5aaca
Dreamwalker <aristojeff@gmail.com>
2019-04-19 00:06:26 +0900

    Implementation Customer js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Customer from "./components/Customer";
+
+const customer = {
+  name: "홍길동",
+  birthday: "911222",
+  gender: "남자",
+  job: "대학생"
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-           
-          </a>
-        </header>
-      </div>
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
     );
   }
 }
