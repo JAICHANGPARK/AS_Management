336f7aa22b0d7ebd558399e08b791051a79cce14
Dreamwalker <aristojeff@gmail.com>
2019-04-19 00:22:54 +0900

    map ~

--- src/App.js
+++ src/App.js
@@ -3,22 +3,77 @@ import logo from "./logo.svg";
 import "./App.css";
 import Customer from "./components/Customer";
 
-const customer = {
-  name: "홍길동",
-  birthday: "911222",
-  gender: "남자",
-  job: "대학생"
-};
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/any",
+    name: "홍길동",
+    birthday: "911222",
+    gender: "남자",
+    job: "대학생"
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "박보검",
+    birthday: "911222",
+    gender: "남자",
+    job: "대학생"
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/1",
+    name: "송혜교",
+    birthday: "911222",
+    gender: "여자",
+    job: "대학생"
+  }
+];
 
 class App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+        {/* <Customer
+          id={customers[0].id}
+          image={customers[0].image}
+          name={customers[0].name}
+          birthday={customers[0].birthday}
+          gender={customers[0].gender}
+          job={customers[0].job}
+        />
+
+        <Customer
+          id={customers[1].id}
+          image={customers[1].image}
+          name={customers[1].name}
+          birthday={customers[1].birthday}
+          gender={customers[1].gender}
+          job={customers[1].job}
+        />
+
+        <Customer
+          id={customers[0].id}
+          image={customers[0].image}
+          name={customers[0].name}
+          birthday={customers[0].birthday}
+          gender={customers[0].gender}
+          job={customers[0].job}
+        /> */}
+      </div>
     );
   }
 }
