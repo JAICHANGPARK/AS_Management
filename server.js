ca15fe6017e1e30ba3ce1923e8237bbdbd514c69
Dreamwalker <aristojeff@gmail.com>
2019-04-21 17:46:09 +0900

    Implement ?

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const express = require("express");
 
@@ -13,28 +13,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //     res.send({message:'Hello Express ! '});
 // })
 
-const data = fs.readFileSync('./database.json');
+const data = fs.readFileSync("./database.json");
 const conf = JSON.parse(data);
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-const connection = mysql.createConnection(
-  {
-    host : conf.host,
-    user : conf.user,
-    password : conf.password,
-    port : conf.port,
-    database : conf.database
-  }
-);
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
 connection.connect();
 
+const multer = require("multer");
+const upload = multer({
+  dest: "./upload"
+});
+
 app.get("/api/customers", (req, res) => {
-  connection.query(
-    "SELECT * FROM CUSTOMER",
-    (err, rows, fields) =>{
-      res.send(rows);
-    }
-  )
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+    res.send(rows);
+  });
   // res.send();
   // res.send([
   //   {
@@ -62,6 +62,30 @@ app.get("/api/customers", (req, res) => {
   //     job: "대학생"
   //   }
   // ]);
+});
+
+app.use("/image", express.static("./upload"));
+
+app.post("/api/customers", upload.single("image"), (req, res) => {
+  let sql = "insert into CUSTOMER values(null, ?,?,?,?,?)";
+  let image = "/image/" + req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+
+  console.log(name);
+  console.log(birthday);
+  console.log(gender);
+  console.log(job);
+  console.log(image);
+
+  let params = [image, name, birthday, gender, job];
+  connection.query(sql, params, (err, rows, fields) => {
+    res.send(rows);
+    console.log(err);
+    console.log(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
